709af86a5525cf70ae9644e42ce9cb57ddb88dc7
Serg <kretosergey@gmail.com>
2025-05-12 17:25:55 +0300

    fix and format mainwindow xaml  + add Focusable="False" for theme switch and OpenControlPanelButton

--- SpectrumNet/MainWindow.xaml.cs
+++ SpectrumNet/MainWindow.xaml.cs
@@ -559,7 +559,12 @@ public partial class MainWindow : Window, IAsyncDisposable
 
     private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
     {
-        Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
         e.Handled = true;
+        if (_isClosing || _isDisposed) return;
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = e.Uri.AbsoluteUri,
+            UseShellExecute = true
+        });
     }
 }
